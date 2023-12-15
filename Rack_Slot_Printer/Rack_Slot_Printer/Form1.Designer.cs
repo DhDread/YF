0bca89d6239abdff8a5f77478d607e2462fdfb05
DhDread <33768837+DhDread@users.noreply.github.com>
2023-12-15 12:15:56 -0500

    Add files via upload

---
+++ Rack_Slot_Printer/Rack_Slot_Printer/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace Rack_Slot_Printer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CB_Line = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txt_Ip_Address = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Txt_Start = new System.Windows.Forms.TextBox();
+            this.Txt_End = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Txt_Rack_Id = new System.Windows.Forms.TextBox();
+            this.JNAP_ChkBox = new System.Windows.Forms.CheckBox();
+            this.DPI_ChkBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // CB_Line
+            // 
+            this.CB_Line.FormattingEnabled = true;
+            this.CB_Line.Items.AddRange(new object[] {
+            "Front Left Hand",
+            "Rear Left Hand",
+            "Front Right Hand",
+            "Rear Right Hand"});
+            this.CB_Line.Location = new System.Drawing.Point(196, 108);
+            this.CB_Line.Name = "CB_Line";
+            this.CB_Line.Size = new System.Drawing.Size(182, 33);
+            this.CB_Line.TabIndex = 0;
+            this.CB_Line.SelectedIndexChanged += new System.EventHandler(this.CB_Line_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Line:";
+            // 
+            // Txt_Ip_Address
+            // 
+            this.Txt_Ip_Address.Location = new System.Drawing.Point(492, 108);
+            this.Txt_Ip_Address.Name = "Txt_Ip_Address";
+            this.Txt_Ip_Address.Size = new System.Drawing.Size(194, 31);
+            this.Txt_Ip_Address.TabIndex = 2;
+            this.Txt_Ip_Address.Text = "10.17.98.27";
+            this.Txt_Ip_Address.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Start:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(535, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "End:";
+            // 
+            // Txt_Start
+            // 
+            this.Txt_Start.Location = new System.Drawing.Point(246, 293);
+            this.Txt_Start.Name = "Txt_Start";
+            this.Txt_Start.Size = new System.Drawing.Size(85, 31);
+            this.Txt_Start.TabIndex = 5;
+            this.Txt_Start.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt_End
+            // 
+            this.Txt_End.Location = new System.Drawing.Point(521, 293);
+            this.Txt_End.Name = "Txt_End";
+            this.Txt_End.Size = new System.Drawing.Size(85, 31);
+            this.Txt_End.TabIndex = 6;
+            this.Txt_End.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 68);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "PRINT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(567, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 68);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "CANCEL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(535, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "IP Address:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Rack:";
+            // 
+            // Txt_Rack_Id
+            // 
+            this.Txt_Rack_Id.Location = new System.Drawing.Point(392, 213);
+            this.Txt_Rack_Id.Name = "Txt_Rack_Id";
+            this.Txt_Rack_Id.Size = new System.Drawing.Size(72, 31);
+            this.Txt_Rack_Id.TabIndex = 11;
+            this.Txt_Rack_Id.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // JNAP_ChkBox
+            // 
+            this.JNAP_ChkBox.AutoSize = true;
+            this.JNAP_ChkBox.Location = new System.Drawing.Point(24, 22);
+            this.JNAP_ChkBox.Name = "JNAP_ChkBox";
+            this.JNAP_ChkBox.Size = new System.Drawing.Size(79, 29);
+            this.JNAP_ChkBox.TabIndex = 12;
+            this.JNAP_ChkBox.Text = "JNAP";
+            this.JNAP_ChkBox.UseVisualStyleBackColor = true;
+            // 
+            // DPI_ChkBox
+            // 
+            this.DPI_ChkBox.AutoSize = true;
+            this.DPI_ChkBox.Location = new System.Drawing.Point(24, 66);
+            this.DPI_ChkBox.Name = "DPI_ChkBox";
+            this.DPI_ChkBox.Size = new System.Drawing.Size(101, 29);
+            this.DPI_ChkBox.TabIndex = 13;
+            this.DPI_ChkBox.Text = "600 DPI";
+            this.DPI_ChkBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 549);
+            this.Controls.Add(this.DPI_ChkBox);
+            this.Controls.Add(this.JNAP_ChkBox);
+            this.Controls.Add(this.Txt_Rack_Id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Txt_End);
+            this.Controls.Add(this.Txt_Start);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Txt_Ip_Address);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CB_Line);
+            this.Name = "Form1";
+            this.Text = "WL Rack Slot Printer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CB_Line;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txt_Ip_Address;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Txt_Start;
+        private System.Windows.Forms.TextBox Txt_End;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Txt_Rack_Id;
+        private System.Windows.Forms.CheckBox JNAP_ChkBox;
+        private System.Windows.Forms.CheckBox DPI_ChkBox;
+    }
+}
+
---
+++ Rack_Slot_Printer/Rack_Slot_Printer/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rack_Slot_Printer
+{
+    public partial class Form1 : Form
+    {
+       // public static int Print_Count = 0;
+        public static int Print_Rack_Int = 0; public static string Print_Rack_Str = null;
+        public static string Print_Loc = null;
+        public static string Print_Loc_Line = null;
+        public static string Print_Final = null;
+       // public static string Print_Final = null;
+        public static string IP_Address = "10.17.98.27";
+        public static int Print_Rack_Start_Int = 0; public static string Print_Rack_Start_Str;
+        public static int Print_Rack_End_Int = 0; public static string Print_Rack_End_Str;
+        public static string data;
+        public static string ZPLString = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            values();
+        }
+        private void values()
+        {
+            CB_Line.Text = "- = Select = -";
+            Txt_Ip_Address.Text = IP_Address;
+            Txt_Rack_Id.Text = null;
+            Txt_Start.Text = "1";
+            Txt_End.Text = "24";
+            ZPLString = null;
+            JNAP_ChkBox.Checked = false;
+            DPI_ChkBox.Checked = false;
+        }
+        private void CB_Line_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (JNAP_ChkBox.Checked)
+                {
+                Print_Loc_Line = "J";
+                }
+            else
+                {
+                Print_Loc_Line = "C";
+                 }
+            if (CB_Line.Text == "Front Left Hand")
+            {
+                Print_Loc = Print_Loc_Line + "LHF00";
+                //Txt_Ip_Address.Text = "1.2.3.4";
+            }
+            if (CB_Line.Text == "Rear Left Hand")
+            {
+                Print_Loc = Print_Loc_Line + "LHR00";
+                // Txt_Ip_Address.Text = "2.3.4.5";
+            }
+            if (CB_Line.Text == "Front Right Hand")
+            {
+                Print_Loc = Print_Loc_Line + "RHF00";
+                //Txt_Ip_Address.Text = "3.4.5.6";
+            }
+            if (CB_Line.Text == "Rear Right Hand")
+            {
+                Print_Loc = Print_Loc_Line + "RHR00";
+                //Txt_Ip_Address.Text = "4.5.6.7";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Txt_Rack_Id.Text == "")
+            {
+                MessageBox.Show("Please Enter Rack #", "- = E R R O R = -");
+            }
+            else
+            {
+                Print_Rack_Int = Int32.Parse(Txt_Rack_Id.Text);
+            }
+            Print_Rack_Start_Int = Int32.Parse(Txt_Start.Text);
+            Print_Rack_End_Int = Int32.Parse(Txt_End.Text);
+            if(Print_Rack_Start_Int < 10)
+            {
+                Print_Rack_Start_Str = "0" + Print_Rack_Start_Int.ToString();
+            }
+            else
+            {
+                Print_Rack_Start_Str = Txt_Start.Text;
+            }
+            if(Print_Rack_End_Int < 10)
+            {
+                Print_Rack_End_Str = "0" + Print_Rack_End_Int.ToString();
+            }
+            else
+            {
+                Print_Rack_End_Str = Txt_End.Text;
+            }
+            if(Print_Rack_Int < 10)
+            {
+                Print_Rack_Str = "0" + Print_Rack_Int.ToString();
+            }
+            else
+            {          
+                Print_Rack_Str = Txt_Rack_Id.Text;
+            }
+            if(Print_Loc == null)
+            {
+                MessageBox.Show("Please Pick a Line", "- = E R R O R = -");
+            }
+            else
+            {
+                string Print_Final_Start = Print_Loc + Print_Rack_Str + "-" + Print_Rack_Start_Str;
+                string Print_Final_End = Print_Loc + Print_Rack_Str + "-" + Print_Rack_End_Str;
+                MessageBox.Show("Printing:" + "\n" + Print_Final_Start + "\n" + "to" + "\n" + Print_Final_End);
+                
+                //Z_Print();
+                Counter();
+                values();
+            }           
+        }
+        private void Counter()
+        {
+            for(int count = Print_Rack_Start_Int; count <= Print_Rack_End_Int; count++)
+            {
+                //Print_Final = null;
+                if(count < 10)
+                {
+                    Print_Final = Print_Loc + Print_Rack_Str + "-0" + count;
+                    Console.Write(Print_Final + "\n");
+                    Z_Print();
+                }
+                else
+                {
+                    Print_Final = Print_Loc + Print_Rack_Str + "-" + count;
+                    Console.Write(Print_Final + "\n");
+                    Z_Print();
+                }
+                
+                //Console.WriteLine(count.ToString());
+            }
+        }
+        private void Z_Print()
+        {
+            // Printer IP Address and communication port
+            // location decided by which check from chk_print
+            //ips are global up top
+            int port = 9100;
+
+            // ZPL Command(s)
+            if (DPI_ChkBox.Checked)
+            {
+                ZPLString =
+                @"^XA^PR3^FS^JMA^MUd,200,600^FS^FO45,35^BY3^BXN,18,200,16,16,6,_^FH\^FD"
+                + Print_Final +
+                @"^FS^FO230,35^BY3^BXN,18,200,16,16,6,_^FH\^FD"
+                + Print_Final +
+                @"^FS^FT50,180^A0N,45,45^FD"
+                + Print_Final +
+                @"^FS^MUd,600,600^FS^XZ";
+            }
+            else
+            {
+                ZPLString =
+                @"^XA^PR3^FS^JMA^FO45,35^BY3^BXN,6,200,16,16,6,_^FH\^FD"
+                + Print_Final +
+                @"^FS^FO230,35^BY3^BXN,6,200,16,16,6,_^FH\^FD"
+                + Print_Final +
+                @"^FS^FT50,180^A0N,45,45^FD"
+                + Print_Final +
+                @"^FS^XZ";
+            }
+            try
+            {
+                // Open connection
+                System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient();
+                client.Connect(Txt_Ip_Address.Text, port);
+                // Write ZPL String to connection
+                System.IO.StreamWriter writer =
+                new System.IO.StreamWriter(client.GetStream());
+                writer.Write(ZPLString);
+                writer.Flush();
+                // Close Connection
+                writer.Close();
+                client.Close();
+                //clear data
+                values();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //clear data
+                values();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            values();
+        }
+    }
+}
